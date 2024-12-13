8745bd658d244ffd92ee94a0c422f8f104ff8e77
Kabita-shrestha <kabitashrestha341@gmail.com>
2024-12-13 09:34:26 +0545

    first commit

---
+++ custom.js
@@ -0,0 +1,5 @@
+var name="kabita"
+        var age=18
+        //console.log("My name is "+name + " age"+age)
+        console.log(`my name is ${name}}`)
+        
---
+++ user.js
@@ -0,0 +1,11 @@
+var name="kabita"
+//var email="kabitashrestha@341"
+//var password="123"
+//console.log(`my name is ${name} email${ email} password ${ password}`)
+document.getElementById("info").innerHTML=name
+var rollno='19'
+var email="kabita@gmail.com"
+var address="kathmandu"
+document.getElementById("roll_no").innerHTML=rollno
+document.getElementById("email_id").innerHTML=email
+doccument.getElementById("address").innerHTML=address
